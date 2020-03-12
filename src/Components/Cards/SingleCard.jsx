acc662c6cdf00a6548e4adde8f8da2856bf0a349
Frank Luongo <frank@frankluongo.com>
2020-03-12 09:01:53 -0600

    13: Drag & Drag Constraints

--- src/Components/Cards/SingleCard.jsx
+++ src/Components/Cards/SingleCard.jsx
@@ -6,11 +6,25 @@ const SingleCard = ({ bgColor, heading, image }) => {
   return (
     <Card
       style={{ background: bgColor }}
+      // whileHover={{ scale: 1.2 }}
+      // whileTap={{ background: 'var(--red)' }}
       // whileHover={{ scale: [1.05, 0.9, 1.1, 1.0] }}
-      whileHover={{ scale: 1.2 }}
-      whileTap={{ background: 'var(--red)' }}
-    // There are so many functions!
-    // onHoverEnd={() => console.log('function that runs on hover end')}
+      // There are so many functions!
+      // onHoverEnd={() => console.log('function that runs on hover end')}
+      // drag
+      drag="x"
+      // dragConstraints={{
+      //   top: -100,
+      //   left: -100,
+      //   bottom: 100,
+      //   right: 100,
+      // }}
+      dragConstraints={{
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+      }}
     >
       <h3>{heading}</h3>
       <img src={image} alt="Illustration" />
