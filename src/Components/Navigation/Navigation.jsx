54766fbb36488ee70f3013e7e030d0ac2852b013
Frank Luongo <frank@frankluongo.com>
2020-03-12 08:34:14 -0600

    10: Propagation With Variants

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -5,11 +5,36 @@ import { motion } from "framer-motion"
 import "./Navigation.css"
 
 const variants = {
-  closed: { x: '-100%' },
-  open: { x: 0 }
+  closed: {
+    x: '-100%',
+    transition: {
+      delay: 0.2
+    }
+  },
+  open: { x: 0 },
 }
 
-const Navigation = ({ closeBtnHandler, isNavOpen }) => {
+const linkVariants = {
+  closed: { y: -100, opacity: 0 },
+  open: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      delay: 0.2
+    }
+  },
+}
+
+const NavLink = ({ link }) => {
+  return (
+    <motion.li className="nav-list__item" variants={linkVariants}>
+      <a className="nav-list-item__link" href={link.url}>{link.title}</a>
+    </motion.li>
+  )
+}
+
+
+const Navigation = ({ closeBtnHandler, isNavOpen, links }) => {
   return (
     <motion.nav
       className="nav"
@@ -18,20 +43,13 @@ const Navigation = ({ closeBtnHandler, isNavOpen }) => {
       animate={isNavOpen ? "open" : "closed"}
       transition={{ damping: 100 }}
     >
-      <button onClick={closeBtnHandler}>&times; Close Menu</button>
+      <button aria-label="Close Menu" className="nav__button" onClick={closeBtnHandler}>
+        <span className="nav-button__close-icon">
+          &times;
+        </span>
+      </button>
       <ul className="nav__list">
-        <li className="nav-list__item">
-          <a className="nav-list-item__link" href="/">Home</a>
-        </li>
-        <li className="nav-list__item">
-          <a className="nav-list-item__link" href="/">About</a>
-        </li>
-        <li className="nav-list__item">
-          <a className="nav-list-item__link" href="/">Work</a>
-        </li>
-        <li className="nav-list__item">
-          <a className="nav-list-item__link" href="/">Contact</a>
-        </li>
+        {links.map((link, index) => <NavLink link={link} key={index} />)}
       </ul>
     </motion.nav>
   )
@@ -43,5 +61,26 @@ If using styled components, you would do...
 const thing = styled(motion.thing)
 
 */
+
+Navigation.defaultProps = {
+  links: [
+    {
+      url: '/',
+      title: 'Home'
+    },
+    {
+      url: '/',
+      title: 'About'
+    },
+    {
+      url: '/',
+      title: 'Work'
+    },
+    {
+      url: '/',
+      title: 'Contact'
+    },
+  ]
+}
 
 export default Navigation
